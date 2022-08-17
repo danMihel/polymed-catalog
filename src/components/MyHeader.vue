5ca618e2f9af45d02a1fe60227eea8e81591cc17
danMihel <d.mihel78@gmail.com>
2022-08-17 16:41:04 +0300

    main

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -10,7 +10,13 @@
           <li>Интернет магазин</li>
           <MySelect :options="['О компании', 'Что-то еще']" />
         </div>
-        <MySelect class="text-header__phone" :options="['&nbsp;8 (800) 33-33-299', '+7 (495) 150-80-32']" />
+        <div class="header-phone_wraper">
+          <img src="@/assets/v.png" />
+          <MySelect
+            class="text-header__phone"
+            :options="['&nbsp;8 (800) 33-33-299', '+7 (495) 150-80-32']"
+          />
+        </div>
         <div class="text-header__block">
           <li>Адреса</li>
           <img src="@/assets/nav.png" />
@@ -21,12 +27,16 @@
       <div class="search-line">
         <img class="logo" src="@/assets/logo.png" />
         <div class="search-input">
-          <input class=" " type="search" placeholder="Быстрый поиск нужного исследования..." />
+          <input
+            class=" "
+            type="search"
+            placeholder="Быстрый поиск нужного исследования..."
+          />
           <img class="search-btn" src="@/assets/Search-icon.png" />
         </div>
         <img class="search-icon" src="@/assets/man.png" />
         <img class="search-icon" src="@/assets/cart.png" />
-        <MyButton :title="'Результаты'"/>
+        <MyButton :title="'Результаты'" />
       </div>
     </div>
     <div class="submenu-container">
@@ -38,7 +48,7 @@
           <li>Запись к врачу</li>
           <li>Самодиагностика</li>
           <li>Акции</li>
-          <MySelect :options="['Еще',]" />
+          <MySelect :options="['Еще']" />
         </nav>
         <div class="submenu">
           <img src="@/assets/describe.png" />
@@ -95,12 +105,27 @@ li {
 }
 
 .text-header__phone {
+  padding-right: 5px;
   font-family: "PT Root UI" !important;
   font-style: normal !important;
   font-weight: 500 !important;
   font-size: 20px !important;
   line-height: 120% !important;
   color: #ff1b7a !important;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+.header-phone_wraper {
+  display: flex;
+  align-items: center;
+}
+.header-phone_wraper img {
+  position: relative;
+  z-index: -1;
+  left: 165px;
+  height: 8px;
+  height: 7px;
 }
 
 .text-header__block {
@@ -133,7 +158,7 @@ li {
 
 .search-input {
   display: flex;
-  border: 1px solid #FF1B7A;
+  border: 1px solid #ff1b7a;
   width: 49%;
   height: 44px;
   border-radius: 8px;
@@ -157,7 +182,7 @@ li {
 }
 
 .submenu-container {
-  background: #F7F9FE;
+  background: #f7f9fe;
   margin-top: 24px;
   padding-top: 24px;
   padding-bottom: 24px;
@@ -165,7 +190,6 @@ li {
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-
 }
 
 .submenu {
@@ -182,12 +206,12 @@ li {
 }
 
 .submenu-container li {
-  font-family: 'PT Root UI';
+  font-family: "PT Root UI";
   font-style: normal;
   font-weight: 400;
   font-size: 15px;
   line-height: 120%;
-  color: #1A2A45;
+  color: #1a2a45;
 }
 
 .submenu-container li:not(:last-child) {
@@ -203,16 +227,14 @@ li {
   padding-left: 30px;
   padding-right: 30px;
   margin: auto;
-
-
 }
 
 .crumbs {
-  color: #8790AE;
+  color: #8790ae;
   display: flex;
   flex-direction: row;
   align-items: center;
-  font-family: 'PT Root UI';
+  font-family: "PT Root UI";
   font-style: normal;
   font-weight: 500;
   font-size: 12px;
@@ -225,6 +247,6 @@ li {
 }
 
 .crumbs-active {
-  color: #00B4FF;
+  color: #00b4ff;
 }
 </style>
